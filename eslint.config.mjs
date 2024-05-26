b2d7c0a415f0fcf55ed8ad40ab735e1ce696526c
kanats <kanats007m@gmail.com>
2024-05-26 19:44:28 +0900

    change languageOptions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
-import { env } from "process";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /** @see https://typescript-eslint.io/getting-started */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    env: {
-      node: true,
-    },
     rules: {
-      "no-console": "warn",
-    }
+      'no-console': 'warn',
+    },
   },
   /** @see https://github.com/prettier/eslint-config-prettier?tab=readme-ov-file#installation */
   eslintConfigPrettier,
