fa132adc08f90d0202ad551be17e02d877b07d28
kanats <kanats007m@gmail.com>
2024-05-27 23:16:16 +0900

    format vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
-            fileName: 'time-inputter.min',
-            formats: ['es'],
-        },
-        outDir: 'dist',
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'time-inputter.min',
+      formats: ['es'],
     },
-    plugins: [dts()],
-  })
+    outDir: 'dist',
+  },
+  plugins: [dts()],
+});
