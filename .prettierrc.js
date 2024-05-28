b2671b4994e050124d6a8e5077671bf99d191487
kanats <kanats007m@gmail.com>
2024-05-28 09:17:59 +0900

    change file extension

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  printWidth: 80,
+};
+
+export default config;
